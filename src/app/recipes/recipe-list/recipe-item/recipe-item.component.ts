6a15a844358b109bef5eaa734b4f5fad6a44034e
mughees605 <mughees605@gmail.com>
2017-06-27 06:16:52 +0500

    bettering cross component communication

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import {Recipe} from "../../recipe.model"
+import { RecipeService } from '../../recipe.service' 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-@Input() recipe:Recipe[] 
+@Input() recipe:Recipe
+  constructor(private recipeService:RecipeService) { }
 
-@Output() recipeSelected = new EventEmitter<void>()
 
 onSelected(){
-  this.recipeSelected.emit();
+   this.recipeService.recipeSelected.emit(this.recipe)
 }
-  constructor() { }
 
   ngOnInit() {
   }
