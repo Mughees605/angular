c0440ca07a353d60aace892044610a25a1a82b0f
mughees605 <mughees605@gmail.com>
2017-07-15 15:29:06 +0500

    adding to shopping list completed

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -8,12 +8,18 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients:Ingredient[];
+  ingredients: Ingredient[];
 
-  constructor(private shoppingService:ShoppingListService) { }
+  constructor(private shoppingService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shoppingService.getIngredients()
+    this.shoppingService.ingredientsChanged
+      .subscribe(
+      (ingredients:Ingredient[]) => {
+            this.ingredients = ingredients
+       }
+      )
   }
 
 }
--- src/app/shopping-list/shoppingList.service.ts
+++ src/app/shopping-list/shoppingList.service.ts
@@ -2,7 +2,6 @@ import { EventEmitter } from '@angular/core'
 import { Ingredient } from '../shared/ingredient.model'
 export class ShoppingListService{
     public ingredientsChanged = new EventEmitter<Ingredient[]>()
-
     private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -11,7 +10,9 @@ export class ShoppingListService{
      return this.ingredients.slice();
  }
  addIngredient(ing:Ingredient){
+
      this.ingredients.push(ing);
+     this.ingredientsChanged.emit(this.ingredients.slice());
  }
  getIngredientFromRecipeDetail(ing:Ingredient[]){
      ing.map((ing:Ingredient)=>{
